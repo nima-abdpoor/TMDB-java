fc46cb6ab178304555a5c206fc8c17dd1f01275b
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-10 11:30:15 +0430

    adding base url and api key for api

---
+++ app/src/main/java/com/chinachino/mvvm/Utils/Constants.java
@@ -0,0 +1,7 @@
+package com.chinachino.mvvm.Utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    public static final String API_KEY = "";
+}
