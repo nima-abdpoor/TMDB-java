1de8f78cfffd53ea52ff446ec14bab0681010a5e
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-11 10:37:00 +0430

    RecipeResponse RecipeSearchResponse api interface all added

--- app/src/main/java/com/chinachino/mvvm/Requests/RecipeAPI.java
+++ app/src/main/java/com/chinachino/mvvm/Requests/RecipeAPI.java
@@ -1,4 +1,22 @@
 package com.chinachino.mvvm.Requests;
 
+import com.chinachino.mvvm.Requests.Responses.RecipeSearchResponse;
+import com.chinachino.mvvm.models.Recipe;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeAPI {
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchResponses(
+            @Query("q") String q,
+            @Query("page") String page
+    );
+
+    @GET("api/get")
+    Call<Recipe> getRecipeResponses(
+            @Query("rid") String id
+    );
+
 }
---
+++ app/src/main/java/com/chinachino/mvvm/Requests/Responses/RecipeSearchResponse.java
@@ -0,0 +1,34 @@
+package com.chinachino.mvvm.Requests.Responses;
+
+import com.chinachino.mvvm.models.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RecipeSearchResponse {
+
+    @SerializedName("count")
+    @Expose
+    private int count;
+
+    @SerializedName("recipe")
+    @Expose
+    private List<Recipe> recipes;
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeSearchResponse{" +
+                "count=" + count +
+                ", recipes=" + recipes +
+                '}';
+    }
+}
