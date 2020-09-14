9be2871c6fff2c25384f73b5c7d78ddd4cf4eed0
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-14 18:38:15 +0430

    BaseActivity updated to version that holds fragment and nav_graph

--- app/src/main/java/com/chinachino/mvvm/BaseActivity.java
+++ app/src/main/java/com/chinachino/mvvm/BaseActivity.java
@@ -1,22 +1,14 @@
 package com.chinachino.mvvm;
 
-import android.widget.FrameLayout;
-import android.widget.ProgressBar;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public abstract class BaseActivity extends AppCompatActivity {
-    private ProgressBar progressBar;
+
     @Override
-    public void setContentView(int layoutResID) {
-        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
-        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
-        progressBar  = constraintLayout.findViewById(R.id.progress_bar);
-        getLayoutInflater().inflate(layoutResID,frameLayout,true);
-        super.setContentView(layoutResID);
-    }
-    public void ShowProgressBar(boolean visibility){
-        progressBar.setVisibility(visibility ? progressBar.VISIBLE : progressBar.INVISIBLE);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
     }
 }
